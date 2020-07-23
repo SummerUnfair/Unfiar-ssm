3952b6684efb76122a26b892a0ae743c62839ac6
ferao <928971634@qq.com>
2020-07-23 11:20:33 +0800

    代码逻辑优化2020/07/23

--- src/main/java/com/unfair/testInterface/ServiceJunitTest.java
+++ src/main/java/com/unfair/testInterface/ServiceJunitTest.java
@@ -3,6 +3,7 @@ package com.unfair.testInterface;
 import com.unfair.api.dto.UserDTO;
 import com.unfair.api.vo.UserVO;
 import com.unfair.service.UserService;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,6 +13,16 @@ public class ServiceJunitTest {
 
     public static void main (String[] args)
     {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("spring-common.xml");
+        UserService user = (UserService) ac.getBean("userService");
+        UserDTO userVO1 = new UserDTO();
+        List<UserVO> all = user.findAll(userVO1);
+        for (UserVO userVO : all) {
+            System.out.println(userVO);
+        }
+    }
+    @Test
+    public void userServiceTest(){
         ApplicationContext ac = new ClassPathXmlApplicationContext("spring-common.xml");
         UserService user = (UserService) ac.getBean("userService");
         UserDTO userVO1 = new UserDTO();
