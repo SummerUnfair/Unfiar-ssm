a9eabd9c732cd132f0f20f0c1b74f5f77fbc9ff5
SummerUnfair <928971634@qq.com>
2020-08-15 21:18:30 +0800

    代码逻辑优化2020/8/15

---
+++ src/main/java/com/unfair/bootstrap/bizBootstrap.java
@@ -0,0 +1,29 @@
+package com.unfair.bootstrap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Service;
+
+/**
+ * biz引导程序<br>
+ * 〈功能详细描述〉
+ *
+ * @author fenghao
+ * @since 1.0.0
+ */
+@Service("bizBootstrap")
+public class bizBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(bizBootstrap.class);
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if (event.getApplicationContext().getParent() == null) {   //root application context 没有parent
+            //其它逻辑
+
+        }
+        LOGGER.info("==========系统启动成功==========");
+    }
+}
